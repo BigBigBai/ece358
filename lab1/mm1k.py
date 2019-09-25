e4038f6e8faeb601f6652b7c89f071c9e047f63c
nsblake <nickblake_ftw@yahoo.com>
2019-09-25 19:55:10 -0400

    CSV write for M/M/1/K

--- lab1/mm1k.py
+++ lab1/mm1k.py
@@ -4,8 +4,14 @@
 
 from classes.event_queue import EventQueue
 
-print("Format: K, rho, E[N], P_IDLE, P_LOSS")
+file = 'mm1k.csv'
+headers = ['K', 'rho', 'E[N]', 'P_LOSS']
 
+print('Format: ' + ', '.join(headers))
+
+data = []
 for K in [10, 25, 50]:
     mm1k_des = EventQueue(0.5, 1.5, 0.1, K)
-    mm1k_des.run_des()
+    data += mm1k_des.run_des()
+
+write_to_csv(file, headers, data)
