81b1758d62be31b158023a5e56092e97f2cd0541
Brandon Fong <BrandonFong1997@users.noreply.github.com>
2019-10-10 18:45:58 -0400

    finishing first draft of persistent csma cd

--- lab2/classes/lan_des.py
+++ lab2/classes/lan_des.py
@@ -1,3 +1,5 @@
+import time
+
 from classes.arrival_event import ArrivalEvent
 from classes.lan_node import LanNode
 from utils import constants as c, utils
@@ -11,7 +13,7 @@ class LAN_DES:
         self.lan = []
         self.timer = 0
         self.t_prop = c.D / c.S
-        self.t_trans = c.R / c.L
+        self.t_trans = c.L / c.R
 
     def populate_lan(self):
         for i in range(self.N):
@@ -27,31 +29,54 @@ class LAN_DES:
             self.lan.append(LanNode(curr_node_events))
 
     def next_sender(self):
-        next_node_index = -1
+        sender_index = -1
         next_packet_time = self.T
 
         for i in range(self.N):
             if self.lan[i].events[0].event_time < next_packet_time:
                 next_packet_time = self.lan[i].events[0].event_time
-                next_node_index = i
+                sender_index = i
 
-        return next_node_index
+        return sender_index
+
+    # helper function for printing first packet
+    def first_packet(self, sender, overlaps):
+        first_packet = []
+        for i in range(self.N):
+            t_first_bit = self.timer + abs(i - sender) * self.t_prop
+
+            if self.lan[i].events[0].event_time < t_first_bit:
+                overlaps += 1
+
+            first_packet.append(self.lan[i].events[0].event_time)
+        return overlaps, first_packet
 
     def run_des(self):
         self.populate_lan()
 
+        total_overlaps = 0
         collision_occurred = False
+        total_packets = 0
+        successfully_transmitted = 0
+        total_collisions = 0
         while self.timer < self.T:
-            next_node = self.next_sender()
-            self.timer = self.lan[next_node].events[0].event_time
+            sender = self.next_sender()
+            # print('Next sender: node', sender)
+            self.timer = self.lan[sender].events[0].event_time
+
+            curr_collisions = 0
+            # total_overlaps, first_packet = self.first_packet(sender, total_overlaps)
+            # print(total_overlaps, first_packet)
 
             for i in range(self.N):
-                if i == next_node:
+                if i == sender:
                     continue
-                t_first_bit = self.timer + abs(i - next_node) * self.t_prop
+                t_first_bit = self.timer + abs(i - sender) * self.t_prop
                 t_last_bit = t_first_bit + self.t_trans
 
                 if t_first_bit < self.lan[i].events[0].event_time < t_last_bit:
+                    # print(t_first_bit, t_last_bit)
+                    # print(i, self.lan[i].events[0].event_time)
                     for event in self.lan[i].events:
                         if event.event_time > t_last_bit:
                             break
@@ -60,21 +85,49 @@ class LAN_DES:
                 elif self.lan[i].events[0].event_time < t_first_bit:
                     collision_occurred = True
                     self.lan[i].collisions += 1
+                    curr_collisions += 1
+                    # print('Node', sender, 'collided with node', i)
 
-                    i_backoff = utils.get_exponential_backoff()
+                    i_backoff = utils.get_exponential_backoff(self.lan[i].collisions)
 
+                    i_waiting_start = self.lan[i].events[0].event_time
                     for event in self.lan[i].events:
-                        if event.event_time > t_last_bit + i_backoff:
+                        if event.event_time > i_waiting_start + i_backoff:
                             break
-                        event.event_time = t_last_bit + i_backoff
+                        event.event_time = i_waiting_start + i_backoff
 
             if collision_occurred:
-                self.lan[next_node].collisions += 1
-                curr_backoff = utils.get_exponential_backoff()
+                # print(curr_collisions)
+                collision_occurred = False
+                self.lan[sender].collisions += 1
+                total_collisions += 1
+                # print(self.lan[sender].collisions)
+                curr_backoff = utils.get_exponential_backoff(self.lan[sender].collisions)
 
-                for event in self.lan[i].events:
-                    if event.event_time > t_last_bit + curr_backoff:
+                curr_waiting_start = self.lan[sender].events[0].event_time
+                for event in self.lan[sender].events:
+                    if event.event_time > curr_waiting_start + curr_backoff:
                         break
-                    event.event_time = t_last_bit + curr_backoff
+                    event.event_time = curr_waiting_start + curr_backoff
+                    
+                if self.lan[sender].collisions > c.K_max:
+                    # print('Packet dropped!')
+                    self.lan[sender].collisions = 0
+                    self.lan[sender].events.popleft()
+                    
+            else:
+                self.lan[sender].collisions = 0
+                self.lan[sender].events.popleft()
+                successfully_transmitted += 1
+
+            total_packets += 1
+            # print(self.timer)
+
+        print('N =', self.N)
+        print('Successfully transmitted:', successfully_transmitted)
+        print('Total packets:', total_packets)
+        print('Efficiency:', successfully_transmitted * 1.0 / total_packets)
+        print('Total collisions', total_collisions)
+
                 
 
