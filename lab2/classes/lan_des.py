16780d0e2e186847121893b1de390c71c3252078
nsblake <nickblake_ftw@yahoo.com>
2019-11-06 13:06:53 -0500

    Events are not processed faster than saturation time

--- lab2/classes/lan_des.py
+++ lab2/classes/lan_des.py
@@ -13,6 +13,7 @@ class LAN_DES:
         self.lan = []
         self.timer = 0
         self.dropped_packets = 0
+        self.total_packets = 0
 
     def populate_lan(self):
         for i in range(self.N):
@@ -47,6 +48,7 @@ class LAN_DES:
                 node.busy_count = 0
                 node.events.popleft()
                 self.dropped_packets += 1
+                self.total_packets += 1
                 return
         else:
             backoff_time = 0
@@ -78,17 +80,19 @@ class LAN_DES:
         self.populate_lan()
 
         collision_occurred = False
-        total_packets = 0
         successfully_transmitted = 0
         total_collisions = 0
+        last_successful = 0
+        last_successful_node = -1
 
         while self.timer < self.T:
             sender = self.next_sender()
             if sender < 0:
                 break
-            if self.lan[sender].events[0].event_time - self.timer < c.t_trans:
-                print(self.lan[sender].events[0].event_time - self.timer)
+            # if self.lan[sender].events[0].event_time - self.timer < c.t_trans:
+            #     print(self.lan[sender].events[0].event_time - self.timer)
             self.timer = self.lan[sender].events[0].event_time
+            # print(self.timer)
             self.lan[sender].busy_count = 0
 
             curr_collisions = 0
@@ -105,7 +109,7 @@ class LAN_DES:
                     curr_collisions += 1
                     self.handle_collision(self.lan[i], t_last_bit)
 
-                    total_packets += 1
+                    self.total_packets += 1
 
             if collision_occurred:
                 collision_occurred = False
@@ -114,20 +118,25 @@ class LAN_DES:
                     
             else:
                 for i in range(self.N):
-                    if i == sender:
-                        continue
                     t_first_bit = self.timer + abs(i - sender) * c.t_prop
                     t_last_bit = t_first_bit + c.t_trans
 
+                    # if i == sender:
+                    #     print('uh-oh')
+
                     # If line is busy, node will wait to transmit
-                    if t_first_bit < self.lan[i].events[0].event_time < t_last_bit:
+                    if t_first_bit <= self.lan[i].events[0].event_time < t_last_bit:
                         self.update_node_event_times(self.lan[i], t_last_bit)
 
+                # if self.timer - last_successful < c.t_trans:
+                #     print(self.timer - last_successful)
                 self.lan[sender].collisions = 0
                 self.lan[sender].events.popleft()
                 successfully_transmitted += 1
+                last_successful = self.timer
+                last_successful_node = sender
 
-            total_packets += 1
+            self.total_packets += 1
             total_collisions += curr_collisions
 
         leftovers = 0
@@ -137,8 +146,8 @@ class LAN_DES:
 
         print('N =', self.N)
         print('Successfully transmitted:', successfully_transmitted)
-        print('Total packets:', total_packets)
-        print('Efficiency:', successfully_transmitted * 1.0 / total_packets)
+        print('Total packets:', self.total_packets)
+        print('Efficiency:', successfully_transmitted * 1.0 / self.total_packets)
         print('Throughput:', successfully_transmitted * c.L / self.T)
         print('Total collisions:', total_collisions)
         print('Dropped packets:', self.dropped_packets)
